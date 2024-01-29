1f679d13df9ff6ee38803b1c6ccd39bb115493fa
lovent <vuongthai95@gmail.com>
2024-01-29 20:26:15 +0700

    mvc oop lesson 4

--- mvc_oop/app/Controllers/Admin/ProductController.php
+++ mvc_oop/app/Controllers/Admin/ProductController.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Controllers\Admin;
 
+use App\Controllers\BaseController;
 use App\Models\Product;
+use App\Traits\ProductTrait; 
 
-class ProductController {
+class ProductController extends BaseController {
     protected $productModel;
 
     public function __construct() {
@@ -13,15 +15,19 @@ class ProductController {
     //lấy toàn bộ sản phẩm
     public function getAllProduct() {
         //xử lý logic, validate dữ liệu
+        $title = "Danh sách sản phẩm";
         $products = $this->productModel->listProduct();
-        include "app/Views/products.php";
+        // include "app/Views/products.php";
+        $this->render('products.products',compact('products','title'));
     }
     public function getTopTenProduct() {
         
     }
     public function edit($id) {
         $product = $this->productModel->getById($id);
-        include "app/Views/edit.php";
+        // include "app/Views/edit.php";
+        $title="Sửa sản phẩm";
+        $this->render('products.edit',compact('product', 'title'));
     }
     public function update($id) {
         var_dump($_POST);
--- mvc_oop/app/Views/products/edit.blade.php
+++ mvc_oop/app/Views/products/edit.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sửa thông tin sản phẩm</title>
-</head>
-<body>
+@extends('layouts.admin')
+@section('content')
     <form action="<?=route('/products/'.$product['id'].'/update')?>" method="POST">
         <label for="name">Name</label>
         <input type="text" name="name" value="<?=$product['name']?>">
@@ -22,5 +15,4 @@
         <br>
         <input type="submit" name="update" value="Update">
     </form>
-</body>
-</html>
+@endsection
